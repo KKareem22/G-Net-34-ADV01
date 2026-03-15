30b55e40e9a4c63c06a327ed8c9a7d5a35327630
Kareem <krda4969@gmail.com>
2026-03-15 05:31:53 +0200

    Q03

---
+++ G-Net-34-ADV01/Pair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_34_ADV01
+{
+    internal class Pair<TKey,Tvalue>
+    {
+        public Pair(TKey key, Tvalue value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public TKey Key { get; set; }
+        public Tvalue Value { get; set; }
+        public override string ToString() => $"Key :{Key} ,Value :{Value}";
+        
+    }
+}
--- G-Net-34-ADV01/Program.cs
+++ G-Net-34-ADV01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace G_Net_34_ADV01
 {
@@ -29,8 +30,17 @@ namespace G_Net_34_ADV01
             //}
 
             #endregion
+            #region Q03
+            //Q3:What are multiple type parameters? Write Pair<TKey, TValue>?
+            //Multiple Type Parameters in Generics mean that a class, method, interface, or delegate can use more than one generic type instead of a single type.
+            //They allow the code to work with multiple data types at the same time.
 
 
-}
+            //Pair<int, string> pair01 = new(55,"Kareem");
+            //Console.WriteLine($"Pair01 -> {pair01}");
+
+            #endregion
+
+        }
     }
 }
