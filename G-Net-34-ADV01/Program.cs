0693bd8df32b433ef264cef2497c2e4d686ceace
Kareem <krda4969@gmail.com>
2026-03-15 04:34:33 +0200

    Q01

--- G-Net-34-ADV01/Program.cs
+++ G-Net-34-ADV01/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            //What is a generic class? Why use generics?
+            //Generic Class:uses type parameters that are replaced with actual types when you create an instance. The type parameter T acts as a placeholder.
+            //Why use :
+            //1)IntelliSense.
+            //2)Code Reuse.
+            //3)Performance
+            //4)Type Safety
+            #endregion
+
+
+
         }
     }
 }
